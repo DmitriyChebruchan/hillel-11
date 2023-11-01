aa33425fc7105f0975390740589e80964e91c2fb
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-01 21:48:46 +0200

    repo is cloned

--- hillel_11/urls.py
+++ hillel_11/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hillel_11 project.
+URL configuration for hilel12 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from exchange.views import main_view
+
+urlpatterns = [path("admin/", admin.site.urls), path("exchange-rates", main_view)]
