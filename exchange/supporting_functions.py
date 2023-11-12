563e4fd69352cc36bfa90544fa30d7fd22e5cd0c
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-12 22:52:48 +0200

    revised form of input information. now only currancy to is added

--- exchange/supporting_functions.py
+++ exchange/supporting_functions.py
@@ -1,21 +1,47 @@
+from django.http import HttpResponse
+from django.utils import timezone
+
 from exchange.forms import UserInputForm
+from exchange.models import Rate
 
 
 def result_calculator(posted_form):
-    return posted_form.cleaned_data["amount"] * 2
+    required_currency_to = posted_form.cleaned_data["currency_to"]
+
+    # rates = (
+    #     Rate.objects.filter(date=timezone.now().date()).order_by("sell").first()
+    # )
+    # to do CORRECT BELOW LOWERS RATE
+    lowest_rate = (
+        Rate.objects.filter(
+            date=timezone.now().date(),
+            currency_to=required_currency_to,
+        )
+        .order_by("sell")
+        .first()
+    )
+
+    if not lowest_rate:
+        print("Not lowest_rate")
+        return HttpResponse("No rate is found today")
+
+    result = str(
+        round(
+            float(posted_form.cleaned_data["amount"]) * float(lowest_rate.sell),
+            2,
+        )
+    )
+
+    return result
 
 
-def info_dict_generator(curr_from=None, curr_to=None):
+def info_dict_generator(curr_to=None):
     if curr_to is None:
-        curr_to = ["USD", "EUR", "GBP"]
-    if curr_from is None:
-        curr_from = ["USD", "EUR", "GBP"]
+        curr_to = ["USD", "EUR"]
 
-    currencies_from = currency_normalizer(curr_from)
     currencies_to = currency_normalizer(curr_to)
     form = UserInputForm(
         initial={
-            "currencies_from": currencies_from,
             "currencies_to": currencies_to,
         }
     )
